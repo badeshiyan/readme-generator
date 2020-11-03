9be41802189d2f74b056be5c47b7862866420c9b
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-11-03 12:26:21 -0500

    continued index prompts updates

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ inquirer
       type: "list",
       message: "Select the preferred license for your project here.",
       name: "license",
-      choices: ["MIT", "Apache 2.0", "GNU"],
+      choices: ["MIT", "Apache 2.0", "GNU", "ISC"],
     },
     {
       type: "input",
@@ -39,20 +39,34 @@ inquirer
       message: "Provide the tests that were employed for your project, if any.",
       name: "tests",
     },
+    {
+      type: "input",
+      message: "Please enter your Github username here.",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "Please enter your email address here.",
+      name: "email",
+    },
   ])
   .then(function (response) {
-    let markdownData = `## ${response.title}
+    let markdownData = `
+    ## ${response.title}
 
     ## Table of Contents
 
-    * [Description](#Description)
-    * [Installation](#Installation)
-    * [License](#License)
-    * [Usage](#Usage)
-    * [Contributing](#Contributing)
-    * [Tests](#Tests)
+    - [Description](#description)
+    - [Installation](#installation)
+    - [License](#license)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
 
 ## Description
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
 ${response.description}
 
 ## Installation
@@ -71,6 +85,12 @@ ${response.contributing}
 ${response.tests}
 
 ## Questions
-${response.questions}`;
+    If you would like to learn more about me, this project or other projects I've completed,
+    please visit my Github profile here: 
+    [Github Profile](https://github.com/${response.username})
+
+    It is understandable that questions may need to be answered regarding the aforementioned -
+    please send all questions to my email address: ${response.email}`;
+
     fs.writeFileSync("newreadme.md", markdownData);
   });
--- newreadme.md
+++ newreadme.md
@@ -1,34 +1,40 @@
-## d
+
+    ## readme
 
     ## Table of Contents
 
-    * [Description](#Description)
-    * [Installation](#Installation)
-    * [License](#License)
-    * [Usage](#Usage)
-    * [Contributing](#Contributing)
-    * [Tests](#Tests)
+    - [Description](#description)
+    - [Installation](#installation)
+    - [License](#license)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
 
 ## Description
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
-d
+description
 
 ## Installation
-
-d
+commands
 
 ## License
-
 MIT
 
 ## Usage
-
-d
+usage
 
 ## Contributing
-
-d
+resources
 
 ## Tests
+tests
 
-d
+## Questions
+    If you would like to learn more about me, this project or other projects I've completed,
+    please visit my Github profile here: 
+    [Github Profile](https://github.com/badeshiyan)
+
+    It is understandable that questions may need to be answered regarding the aforementioned -
+    please send all questions to my email address: badeshiyan@gmail.com
